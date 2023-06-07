33e1c89a3796334089bedec9bade2fa9578b88cd
abdulla <mr.ad1992@mail.ru>
2023-06-07 14:18:04 +0300

    добавил задачу 5

---
+++ простые здачи/задача 5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 5: Напишите программу, которая на вход принимает одно число (N), а на выходе прказывает все целые числа
+// от -N до N.
+// 4-> "-4;-3;-2;-1;0;1;2;3;4"
+
+Console.WriteLine("Enter your number N: ");
+int N = int.Parse(Console.ReadLine()!);
+int k = -N;
+while (k <= N)
+{
+    Console.Write($"{k} ");
+    k++;
+}
