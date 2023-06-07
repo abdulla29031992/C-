cf8a643badeca64641f48d17cd199a7dfd5479cb
abdulla <mr.ad1992@mail.ru>
2023-06-07 13:59:38 +0300

    добавил задачу 1

---
+++ простые здачи/задача 1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите прогрпмму, котрое на вход принимаеет два числа 
+// и проверяет, является ли первое квадратом второго
+// а=25, b=5 -> да
+
+Console.Clear();
+
+Console.Write("Введите число a ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b ");
+int b = int.Parse(Console.ReadLine()!);
+if (b * b == a)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
