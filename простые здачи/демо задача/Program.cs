5b1e81fa20ffd983cb90505e79bbcdf52684872a
abdulla <mr.ad1992@mail.ru>
2023-06-07 13:51:31 +0300

    добавил демо-задачу

---
+++ простые здачи/демо задача/Program.cs
@@ -0,0 +1,21 @@
+﻿// Демо-задача: Напишите программу которое на вход принимает число  и выдает его квадрат 
+// (число умноженное на самого себя)
+// 2 -> 4
+// 3 -> 9
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int sqr = number * number;
+double sqr2 = Math.Pow(number, 2);
+Console.WriteLine($"Квадрат {number} равен {sqr}");
+Console.WriteLine($"Квадрат {number} равен {sqr2}");
+
+
+
+
+
+
+
