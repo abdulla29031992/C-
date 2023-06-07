95ad7fcc43714217d0a64e3403428c155e5d68cd
abdulla <mr.ad1992@mail.ru>
2023-06-07 14:29:30 +0300

    добавил задачу 7

---
+++ простые здачи/задача 7/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 7: Напишите программу, которое на входе принимает трехзначное число, 
+// а на выходе выдает последнее
+// 456 -> 6
+
+Console.Clear();
+
+Console.Write("Enter your number number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int n = number % 10;
+
+Console.WriteLine($"{n}");
