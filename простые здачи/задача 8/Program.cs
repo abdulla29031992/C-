e36c764c8449b4e30cfd533173f76fc65a26db7e
abdulla <mr.ad1992@mail.ru>
2023-06-10 10:38:37 +0300

    добавил задачу 8

---
+++ простые здачи/задача 8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу которое на вход принимает число N,
+//  а на выходе показывает все четные числа от 1 до N 
+//  5->2,4
+
+Console.Clear();
+
+Console.Write("print your number N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int a = 1;
+
+do 
+{
+    if (a % 2 == 0)
+    {
+         Console.WriteLine($"{a} ");
+    }
+    a++;
+}
+while (a < N);
