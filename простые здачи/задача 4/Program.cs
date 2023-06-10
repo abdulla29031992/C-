b4ea633bc66f14eb2dd72545530d09f7bb271c3c
abdulla <mr.ad1992@mail.ru>
2023-06-10 10:19:20 +0300

    добавил задачу 4

---
+++ простые здачи/задача 4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 4: Напишите программу, котрое на вход принимет три числа и выдает максимальное
+// a = 5; b = 7 С = 8 -> max = 8
+
+Console.Clear();
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число c: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max= ");
+Console.WriteLine(max);
