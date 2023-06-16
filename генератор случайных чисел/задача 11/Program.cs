c0b40447e34cc5b574b8b871f9e545fec77856f1
abdulla <mr.ad1992@mail.ru>
2023-06-16 13:39:43 +0300

    изменил задачу 11

---
+++ генератор случайных чисел/задача 11/Program.cs
@@ -0,0 +1,16 @@
+﻿// / Задача 11: Напишите программу, кторая выводит случайное трехзначное число и удаляет вторую цифру.
+// 456->46
+
+Console.Clear();
+
+int num = new Random().Next(10, 999);
+
+ Console.WriteLine(num);
+
+int a1 = num / 100;
+
+int a2 = num % 10;
+
+int res = a1 * 10 + a2;
+
+ Console.WriteLine($"{res}");
