aabbb5968c17d82c50bf0548ab7026eda2f34b10
abdulla <mr.ad1992@mail.ru>
2023-06-16 14:01:54 +0300

    изменил задачу 14

---
+++ простые здачи/задача 14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 14: Напишите программу которое на вход принимает число и проверяет, 
+// кратно ли одновременно 7 и 23
+// 14-> нет;
+// 161-> да. 
+
+Console.Clear();
+
+Console.WriteLine("print your number: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.Write("{yes}");
+}
+else
+{
+    Console.Write("{no}");
+}
