acba046dfecb0ce16a7f636c42f0dc03b4a6039c
abdulla <mr.ad1992@mail.ru>
2023-06-18 14:17:28 +0300

    добавил задачу 21/v2

---
+++ простые здачи/задача 21/второй вариант/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Введите ккординату точки x1: ");
+int x1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите ккординату точки y1: ");
+int y1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите ккординату точки x2: ");
+int x2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите ккординату точки y2: ");
+int y2 = int.Parse(Console.ReadLine()!);
+
+double x = Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y2, 2);
+double result = Math.Sqrt(x);
+
+Console.Write(Math.Round(result, 2));
