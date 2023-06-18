5da1344c82f3cf67071483b03adcfbef2ea325ac
abdulla <mr.ad1992@mail.ru>
2023-06-18 13:47:37 +0300

    добавил задачу 18/v2

---
+++ простые здачи/задача 18/второй вариант/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.Write("Введите номер четверти n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+switch (n)
+{
+    case 1:
+        Console.Write("x > 0; y > 0");
+        break; 
+    case 2:
+        Console.Write("x < 0; y > 0");
+        break; 
+    case 3:
+        Console.Write("x < 0; y < 0");
+        break;
+    case 4:
+        Console.Write("x < 0; y < 0");
+        break;
+    default:
+        Console.Write("Ввелите число от 1 до 4");
+        break;
+}    
