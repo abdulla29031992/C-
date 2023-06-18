8dd8c0c3ed070ce365ae81b24813dea9e04ff697
abdulla <mr.ad1992@mail.ru>
2023-06-18 14:12:04 +0300

    добавил задачу 21

---
+++ простые здачи/задача 21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21: Напишите програграмму, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D  пространстве.
+
+Console.Write("Введите ккординату точки x1: ");
+int x1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите ккординату точки y1: ");
+int y1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите ккординату точки x2: ");
+int x2 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите ккординату точки y2: ");
+int y2 = int.Parse(Console.ReadLine()!);
+
+double a = Math.Pow(x2 - x1, 2);
+double b = Math.Pow(y2 - y1, 2);
+
+double c = Math.Sqrt(a + b);
+
+Console.Write(Math.Round(c, 2));
