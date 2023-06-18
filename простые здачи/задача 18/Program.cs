a30f9d5fd69de19a9a2b1d7dfd99fe2814784a7d
abdulla <mr.ad1992@mail.ru>
2023-06-18 13:31:51 +0300

    добавил задачу 18

---
+++ простые здачи/задача 18/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, показываеь диапазон возможных 
+// координат точек в этой четверти (x y)
+
+Console.Clear();
+
+Console.Write("Введите номер четверти n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+
+if (n == 1)
+{
+    Console.Write("x > 0; y > 0");
+}
+else if (n == 2)
+{
+    Console.Write("x < 0; y > 0");
+}
+else if (n == 3)
+{
+    Console.Write("x < 0; y < 0");
+}
+else if (n == 4)
+{
+    Console.Write("x < 0; y < 0");
+}
+else
+{
+    Console.Write("Ввелите число от 1 до 4");
+}
