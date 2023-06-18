80a54b4768ad863b5b53424156c370b9a9cde7b0
abdulla <mr.ad1992@mail.ru>
2023-06-18 13:14:31 +0300

    добавил задачу 17/v2

---
+++ простые здачи/задача 17/второй вариант/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+Console.Write("Введите координату x: ");
+int x = int.Parse(Console.ReadLine()!);
+
+
+Console.Write("Введите координату y: ");
+int y = int.Parse(Console.ReadLine()!);
+
+
+if (y > 0)
+{
+    if (x > 0)
+    Console.Write($"Певая четверть");
+    else
+    {
+        Console.Write("вторая четверть");
+    }
+}
+else 
+{
+    if (x > 0)
+    {
+        Console.Write($"четвертая четверть");
+    }
+    else
+    {
+        Console.Write("третья четверть");
+    }
+}
