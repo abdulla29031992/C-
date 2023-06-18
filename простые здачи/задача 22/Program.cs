a8e7e4c2bb001341429e48d25ba833d087c6cfce
abdulla <mr.ad1992@mail.ru>
2023-06-18 14:23:00 +0300

    добавил задачу 22

---
+++ простые здачи/задача 22/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 22: Напишите программу, которая на вход принимает число (N) и выдает таблицу квадратов чисел
+// от 1 до N
+// 5 -> 1,2,3,16,25
+
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+while (i <= N)
+
+{
+Console.Write($"{Math.Pow(i, 2) + " "}");
+i++;
+}
