2114e7a99e62ed8e841243c31e5bac50c7f075c4
abdulla <mr.ad1992@mail.ru>
2023-06-20 13:46:54 +0300

    закончил задачу 28

---
+++ функция/задача 28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+// 4 -> 24
+
+int GetMultiNumber(int A)
+{
+    int number = 1;
+    for (int i = 1; i <= A; i++)
+    {
+        number = number * i;
+    }
+    return number;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"произведение чисел от 1 до {n} ровно {GetMultiNumber(n)}");
