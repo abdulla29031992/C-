cbd19c79b9ceef8989244d3bc3ead49c1af41155
abdulla <mr.ad1992@mail.ru>
2023-06-20 13:37:19 +0300

    закончил задачу 26/v2

---
+++ функция/задача 26/второй вариант/Program.cs
@@ -0,0 +1,22 @@
+﻿int DigitCount(int a)
+{
+    int count = 0;
+    if (a == 0)
+    {
+        count = 1;
+    }
+    else
+    while(a != 0)
+    {
+        a /= 10;
+        count ++;
+    }
+    return count;
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(DigitCount(N));
