379d461b5a89ac115f5d2bd5682740b2e09f7966
abdulla <mr.ad1992@mail.ru>
2023-06-20 11:56:22 +0300

    добавил задачу 19

--- test/Program.cs
+++ test/Program.cs
@@ -1,16 +1,1 @@
-﻿Console.Clear();
-
-Console.WriteLine("print number n: ");
-int n = int.Parse(Console.ReadLine()!);
-
-int a = 1;
-
-do 
-{
-    if (a % 2 == 0)
-    {
-        Console.WriteLine($"{a} ");
-    }
-    a++;
-}
-while (a < n);
+﻿
