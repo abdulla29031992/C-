9b9fa5cc2f59a79fd3d3c10acd5e4c262c7afd22
abdulla <mr.ad1992@mail.ru>
2023-06-20 13:17:30 +0300

    добавил задачу 26

--- test/Program.cs
+++ test/Program.cs
@@ -1,15 +1,1 @@
-﻿int GetSum(int A)
-{
-    int sum = 0;
-    for (int i = 0; i <= A; i ++)
-    {
-        sum = sum + i;
-    }
-
-    return sum;
-}
-
-Console.WriteLine("введите число n: ");
-int n = int.Parse(Console.ReadLine()!);
-
-Console.WriteLine($"сумма чисел от 1 до n равна {GetSum(n)}");
+﻿
---
+++ функция/задача 26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдает количество цифр в чисел
+// 45 -> 2
+
+int DigitCount(int A)
+
+{
+    int count = 0;
+    while (A != 0)
+    {
+        A /= 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(DigitCount(N));
