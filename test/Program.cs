f3dd9aaf87cb5dba1b1b57596bf4c9195eda4bb5
abdulla <mr.ad1992@mail.ru>
2023-06-20 12:49:38 +0300

    изменил код теста

--- test/Program.cs
+++ test/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿Console.Clear();
+
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
+
+void CheckPalindromicNumber(int number)
+{
+    if (number > 10000)
+    {
+        int div1 = number / 100000;
+        int rem1 = number % 10;
+
+        if (div1 == rem1)
+
+        number = number % 10;
+        
+        int div2 = (number / 100) % 10;
+        int rem2 = number % 10;
+
+        if (div2 == rem2)
+        {
+            Console.WriteLine("yes")
+        }
+    }
+
+   
+}
