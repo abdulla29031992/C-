7b5a587667522ce9dbf387018c8e3fbceea121ba
abdulla <mr.ad1992@mail.ru>
2023-06-20 13:22:40 +0300

    изменил код теста

--- test/Program.cs
+++ test/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿int DigitCount(int A)
+
+{
+    int count = 0;
+    while(A != 0)
+    {
+        A /= 10;
+        count ++;
+    }
+    return count;
+}
+
+Console.Clear();
+
+Console.WriteLine("введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"колличество цифр в числе равно {DigitCount(N)}");
