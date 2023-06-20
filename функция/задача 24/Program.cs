3373d74be8ddaf4be16a3b9b1d53274bdc3a6b54
abdulla <mr.ad1992@mail.ru>
2023-06-20 12:58:39 +0300

    добавил задачу 24

---
+++ функция/задача 24/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 24: Напишите программу котрая на вход принимает число N и выдает сумму чисел от 1 до N
+// 7 -> 28
+
+int GetSum(int N)
+{
+int sum = 0;
+for (int i = 1; i <= N; i++)
+{
+sum += i; // sum = sum + i
+}
+return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма чисел от 1 до {number} равна {GetSum(number)}");
