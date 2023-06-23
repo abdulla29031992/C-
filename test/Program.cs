6246482ca000db7295e95dec3986aaf2f8e72b38
abdulla <mr.ad1992@mail.ru>
2023-06-23 14:00:25 +0300

    добавил задачу 33

--- test/Program.cs
+++ test/Program.cs
@@ -4,23 +4,36 @@ int[] GetArray(int size, int MinValue, int MaxValue)
 {
     int[] result = new int[size];
 
-    for ( int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(MinValue, MaxValue + 1);
+        result[i] = new Random { }.Next(MinValue, MaxValue + 1);
     }
-
     return result;
 }
 
-void SigChange(int[] array)
+bool FindElement(int number, int[] collection)
 {
-    for (int i = 0; i < array.Length; i++)
+    foreach (int el in collection)
     {
-        array[i] *= -1;
+        if (el == number)
+        {
+            return true;
+        }
     }
+    return false;
 }
 
 int[] array = GetArray(12, -9, 9);
 Console.WriteLine(String.Join(", ", array));
-SigChange(array);
-Console.WriteLine(String.Join(", ", array));
+
+Console.WriteLine("ВВЕДИТЕ ЧИСЛО N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+if (FindElement(N, array))
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
---
+++ одномерный массив/задача 33/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присуствует ли 
+// заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+
+
+Console.Clear();
+
+int[] GetArray(int size, int MinValue, int MaxValue)
+{
+    int[] Result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Result[i] = new Random().Next(MinValue, MaxValue + 1);
+    }
+    return Result;
+}
+
+bool FindElement(int number, int[] collection)
+{
+    foreach(int el in collection)
+    {
+        if (el == number)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(", ", array));
+
+Console.Write("введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (FindElement(n, array))
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
