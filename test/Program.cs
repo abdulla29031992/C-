ac0deeb99c1003829bd4a68eb63943924d144b3d
abdulla <mr.ad1992@mail.ru>
2023-06-23 13:17:14 +0300

    изменил код теста

--- test/Program.cs
+++ test/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿Console.Clear();
+
+int[] GetArray(int size; int minValue; int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(", ", array));
+
+int positiveSum = 0;
+int negativeSum = 0;
+
+foreach (int el in array)
+{
+    positiveSum += el > 0 ? el : 0;
+    negativeSum += el < 0 ? el : 0;
+}
+
+Console.Write($"сумма положительгых чисел массива ровна {positiviSum} а сумма отрицательных {negativeSum}");
--- одномерный массив/задача 31/Program.cs
+++ одномерный массив/задача 31/Program.cs
@@ -5,14 +5,14 @@ Console.Clear();
 
 int[] GetArray(int size, int minValue, int maxValue)
 {
-int[] result = new int[size];
+    int[] result = new int[size];
 
-for (int i = 0; i < size; i++)
-{
-result[i] = new Random().Next(minValue, maxValue + 1);
-}
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
 
-return result;
+    return result;
 }
 
 int[] array = GetArray(12, -9, 9);
@@ -23,12 +23,12 @@ int negativeSum = 0;
 
 foreach (int element in array)
 {
-positiveSum += element > 0 ? element : 0;
-// if (el > 0)
-// {
-// positiveSum += el;
-// }
-negativeSum += element < 0 ? element : 0;
+    positiveSum += element > 0 ? element : 0;
+    // if (el > 0)
+    // {
+    // positiveSum += el;
+    // }
+    negativeSum += element < 0 ? element : 0;
 }
 
 Console.WriteLine($"Сумма положительных чисел равна {positiveSum}, а отрициальных {negativeSum}");
