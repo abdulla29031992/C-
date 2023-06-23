885f5060b8d31e765294f479cc8798bed3377270
abdulla <mr.ad1992@mail.ru>
2023-06-23 15:13:08 +0300

    изменил код теста

--- test/Program.cs
+++ test/Program.cs
@@ -1,17 +1,35 @@
 ﻿Console.Clear();
 
-int[] Getarray(int size; int MinValue, int MaxValue)
+int[] GetArray(int size, int MinValue, int MaxValue)
 {
     int[] result = new int[size];
 
-    for (int i = 0; i < size, i++)
+    for (int i = 0; i < size; i++)
     {
-        result[i] = new Random().Next(MinValue, MaxValue + 1);
+        result[i] = new Random().Next(MinValue, MaxValue +1);
     }
     return result;
 }
 
+int[] MultiArray(int[] array)
+{
+    int size = array.Length / 2;
 
+    if (array.Length % 2 != 0) size++;
 
-int[] array1 = Getarray(4, 1, 10);
-Console.WriteLine(String.Join("," , array1));
+    int[] result = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length - 1 - i];
+    }
+
+    if (array.Length % 2 != 0)
+    array[size - 1] = array[array.Length / 2];
+    return result;
+}
+
+int[] array1 = GetArray(4, -9, 9);
+Console.WriteLine(String.Join(", ", array1));
+
+int[] array2 = MultiArray(array1);
+Console.WriteLine(String.Join(", ", array2));
