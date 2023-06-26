c4a132c42d6e62b4861dd73313b9f5276820c8b8
abdulla <mr.ad1992@mail.ru>
2023-06-26 15:32:58 +0300

    добавил задачу 34

---
+++ одномерный массив/задача 34/Program.cs
@@ -0,0 +1,30 @@
+﻿// // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// // Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+int CountElement(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) result++;
+    }
+    return result;
+}
+
+int[] array = GetArray(10, 100, 1000);
+Console.WriteLine(String.Join(",", array));
+int end = CountElement(array);
+Console.WriteLine($"колличество четных элементов массива равно {end}");
