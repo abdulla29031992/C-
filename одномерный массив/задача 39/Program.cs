8e93c3b242b38401d7a8808d3416939374cccca8
abdulla <mr.ad1992@mail.ru>
2023-06-28 11:45:16 +0300

    закончил задачу 39

--- одномерный массив/задача 39/Program.cs
+++ одномерный массив/задача 39/Program.cs
@@ -5,3 +5,40 @@
 // [6 7 3 6] -> [6 3 7 6]
 
 
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void ReversArray1(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int temp = inArray[i];
+        inArray[i] = inArray[inArray.Length - 1 - i]; // - элемент с конца; inArray[i] - элемент с начала
+        inArray[inArray.Length - 1 - i] = temp;
+    }
+}
+
+int[] ReversArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
+
+Console.Clear();
+int[] array = GetArray(5, -10, 10);
+Console.WriteLine(string.Join(", ", array));
+ReversArray1(array);
+Console.WriteLine(string.Join(", ", array));
+int[] revered = ReversArray2(array);
+Console.WriteLine(string.Join(", ", revered));
