0589ced151a1e78789b90435e92f19da61528f4e
abdulla <mr.ad1992@mail.ru>
2023-06-28 12:15:27 +0300

    изменил код теста

--- test/Program.cs
+++ test/Program.cs
@@ -1,46 +1,24 @@
-﻿int[] GetArray(int size, int MinValue, int MaxValue)
+﻿Console.Clear();
+
+Console.WriteLine("введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("введите число c: ");
+int c = int.Parse(Console.ReadLine()!);
+
+bool CheckTriangele(int a, int b, int c)
 {
-    int[] result = new int[size];
-
-    for (int i = 0; i < size; i++)
-    {
-        result[i] = new Random().Next(MinValue, MaxValue + 1);
-    }
-
-    return result;
+    return a < b + c && b < a + c && c < a + b;
 }
 
-void ReversArray1(int[] inArray)
+if (CheckTriangele(a, b, c))
 {
-
-    for (int i = 0; i < inArray.Length / 2; i++)
-    {
-        int temp = inArray[i];
-        inArray[i] = inArray[inArray.Length - 1 - i];
-        inArray[inArray.Length - 1 - i] = temp;
-    }
+    Console.WriteLine("Треугольник существует");
 }
-
-int[] ReversArray2(int[] inArray)
+else
 {
-    int[] result = new int[inArray.Length];
-
-    for (int i = 0; i < inArray.Length; i++)
-    {
-        result[i] = inArray[inArray.Length - 1 - i];
-    }
-    return result;
-
+    Console.WriteLine("Треугольника не существует");
 }
-
-
-Console.Clear();
-
-int[] array = GetArray(6, 1, 10);
-Console.WriteLine(String.Join(", ", array));
-
-ReversArray1(array);
-Console.WriteLine(String.Join(", ", array));
-
-int[] revered = ReversArray2(array);
-Console.WriteLine(String.Join(", ", revered));
