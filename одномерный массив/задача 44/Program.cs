7e2b311698c79e94263dba01819517b31d5afc72
abdulla <mr.ad1992@mail.ru>
2023-06-30 06:48:25 +0300

    добавил задачу 44

---
+++ одномерный массив/задача 44/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int[] ArrayFib(int N)
+{
+    int[] array = new int[N];
+
+    array[0] = 0;
+    array[1] = 1;
+
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+
+    return array;
+}
+
+Console.Clear();
+
+Console.Write("enter your number n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write(String.Join(", ", ArrayFib(n)));
