7c1ea9d86cd74f672b12bf7f07f18622393d060c
abdulla <mr.ad1992@mail.ru>
2023-06-30 06:16:27 +0300

    изменил код теста

--- функция/задача 42/Program.cs
+++ функция/задача 42/Program.cs
@@ -1,16 +1,19 @@
-﻿Console.Clear();
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.Clear();
 
 string ConvertDecToBin(int number)
 {
     string result = String.Empty;
-
     while (number != 0)
     {
         result = (number % 2).ToString() + result;
-
         number /= 2;
     }
-
     return result;
 }
 
