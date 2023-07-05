00489c80f1b7cb788d6ef7f04e597792665864de
unknown <mr.ad1992@mail.ru>
2023-07-05 19:38:53 +0300

    добавил задачу 29

---
+++ двумерный массив/задача 49/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты
+
+int[,] GetArray(int row, int col, int minValue, int maxValue)
+{
+    int[,] result = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void GetSqrArray(int[,] array2D)
+{
+    for (int i = 0; i < array2D.GetLength(0); i+= 2)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j+= 2)
+        {
+            array2D[i,j] *= array2D[i,j];
+        }
+    }
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("print row: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("print col: ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array2D = GetArray(row, col, -10, 10);
+PrintArray(array2D);
+Console.WriteLine();
+GetSqrArray(array2D);
+PrintArray(array2D);
