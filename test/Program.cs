5b020a3985edc75ba358ec80febfd62cd9e24003
unknown <mr.ad1992@mail.ru>
2023-07-10 05:59:27 +0300

    добавил задачу 47

--- test/Program.cs
+++ test/Program.cs
@@ -1,36 +1,28 @@
-﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
-// которых оба индекса чётные, и замените эти элементы на их
-// квадраты
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9                         
 
-int[,] GetArray(int row, int col, int minValue, int maxValue)
+double[,] GetArray(int row, int col, double minValue, double maxValue)
 {
-    int[,] result = new int[row, col];
+    double[,] result = new double[row, col];
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
-            result[i, j] = new Random().Next(minValue, maxValue + 1);
+            result[i, j] = Math.Round(minValue + new Random().NextDouble() * (maxValue-minValue), 1);
+
         }
     }
     return result;
 }
 
-void GetSqrArray(int[,] array2D)
-{
-    for (int i = 0; i < array2D.GetLength(0); i+= 2)
-    {
-        for (int j = 0; j < array2D.GetLength(1); j+= 2)
-        {
-            array2D[i,j] *= array2D[i,j];
-        }
-    }
-}
-
-void PrintArray(int[,] inArray)
+void PrintArray(double[,] inArray)
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
     {
-        for(int j = 0; j < inArray.GetLength(1); j++)
+        for (int j = 0; j < inArray.GetLength(1); j++)
         {
             Console.Write($"{inArray[i, j]}\t ");
         }
@@ -38,13 +30,10 @@ void PrintArray(int[,] inArray)
     }
 }
 
-Console.Write("print row: ");
-int row = int.Parse(Console.ReadLine()!);
-Console.Write("print col: ");
-int col = int.Parse(Console.ReadLine()!);
-
-int[,] array2D = GetArray(row, col, -10, 10);
-PrintArray(array2D);
-Console.WriteLine();
-GetSqrArray(array2D);
-PrintArray(array2D);
+Console.Clear();
+Console.Write("enter row: ");
+double row = int.Parse(Console.ReadLine()!);
+Console.Write("enter col: ");
+double col = int.Parse(Console.ReadLine()!);
+double[,] array = GetArray(3, 4, -10, 10);
+PrintArray(array);
---
+++ двумерный массив/задача 47/Program.cs
@@ -0,0 +1,34 @@
+﻿double[,] GetArray(int row, int col, double minValue, double maxValue)
+{
+  double[,] result = new double[row, col];
+
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      result[i, j] = Math.Round(minValue + new Random().NextDouble() * (maxValue - minValue), 1);
+    }
+  }
+  return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("enter row: ");
+double row = int.Parse(Console.ReadLine()!);
+Console.Write("enter col: ");
+double col = int.Parse(Console.ReadLine()!);
+double[,] array = GetArray(3, 4, -10, 10);
+PrintArray(array);
