c83a8b09d9c6eb9ab66d5e049b2885622f7729b6
unknown <mr.ad1992@mail.ru>
2023-07-15 16:37:04 +0300

    изменил код теста

--- test/Program.cs
+++ test/Program.cs
@@ -1,51 +1,52 @@
-﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива.
 
-Console.WriteLine("введите количество строк");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите количество столбцов");
-int m = Convert.ToInt32(Console.ReadLine());
+int[,] GetArray(int m, int n, int minValue, int maxValue)
 
-int[,] numbers = new int[n, m];
-FillArrayRandomNumbers(numbers);
-
-
-for (int j = 0; j < numbers.GetLength(1); j++)
 {
-  double avarage = 0;
-  for (int i = 0; i < numbers.GetLength(0); i++)  
-  {
-    avarage = (avarage + numbers[i, j]);
-  }
-  avarage = avarage / n;
-  Console.Write(avarage + "; ");
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
 }
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SwapTwoRow(int[,] arrayNew)
+{
+    for (int j = 0; j < arrayNew.GetLength(1); j++)
+    {
+        int temp = arrayNew[0, j];
+        arrayNew[0, j] = arrayNew[arrayNew.GetLength(0) - 1, j];
+        arrayNew[arrayNew.GetLength(0) - 1, j] = temp;
+    }
+}
+
+Console.Clear();
+Console.Write("row = ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("col = ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] array2D = GetArray(row, col, -10, 10);
+PrintArray(array2D);
+
 Console.WriteLine();
-PrintArray(numbers);
-
-
-
-void FillArrayRandomNumbers(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next(0, 10);
-    }
-  }
-}
-
-void PrintArray(int[,] array)
-{
-
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    Console.Write("[ ");
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write(array[i, j] + " ");
-    }
-    Console.Write("]");
-    Console.WriteLine("");
-  }
-}
+SwapTwoRow(array2D);
+PrintArray(array2D);
---
+++ двумерный массив/задача 53/Program.cs
@@ -0,0 +1,48 @@
+﻿int[,] GetArray(int row, int col, int minValue, int maxValue)
+{
+  int[,] result = new int[row, col];
+
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Console.Write($"{inArray[i, j]}\t ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void SwapTwoRow(int[,] NewArray)
+{
+  for (int j = 0; j < NewArray.GetLength(1); j++)
+  {
+    int temp = NewArray[0, j];
+    NewArray[0, j] = NewArray[NewArray.GetLength(0) - 1, j];
+    NewArray[NewArray.GetLength(0) - 1, j] = temp;
+  }
+}
+
+
+Console.Clear();
+Console.Write("row = ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("col = ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] array2D = GetArray(row, col, -10, 10);
+PrintArray(array2D);
+
+Console.WriteLine();
+SwapTwoRow(array2D);
+PrintArray(array2D);
