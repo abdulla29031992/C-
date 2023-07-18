9382ff8d8e4c2e64d123f6f467e7d0df39b2de9f
unknown <mr.ad1992@mail.ru>
2023-07-18 14:34:52 +0300

    добавил задачу 54

--- test/Program.cs
+++ test/Program.cs
@@ -1,9 +1,5 @@
-﻿// Задача 57: Составить частотный словарь элементов двумерного массива.
-// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
-
-// Набор данных { 1, 9, 9, 0, 2, 8, 0, 9 } 1, 2, 3 4, 6, 1 2, 1, 6
-// Частотный массив 0 встречается 2 раза 1 встречается 1 раз 2 встречается 1 раз 8 встречается 1 раз 9 встречается 3 раза 1 встречается
-// 3 раза 2 встречается 2 раз 3 встречается 1 раз 4 встречается 1 раз 6 встречается 2 раза
+﻿// задача 54. Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -31,53 +27,34 @@ void PrintArray(int[,] inArray)
   }
 }
 
-int[] Transform2DTo1D(int[,] array2D)
+void SortArray(int[,] array2D)
 {
-  int[] array1D = new int[array2D.GetLength(0) * array2D.GetLength(1)];
-  int k = 0;
-  for (int i = 0; i < array2D.GetLength(0); i++)
-  {
-    for (int j = 0; j < array2D.GetLength(0); j++)
+    for (int i = 0; i < array2D.GetLength(0); i++)
     {
-      array1D[k++] = array2D[i, j];
+        for (int j = 0; j < array2D.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < array2D.GetLength(1); k++)
+            {
+                if (array2D[i,j] < array2D[i,k])
+                {
+                    int temp = array2D[i,j];
+                    array2D[i,j] = array2D[i,k];
+                    array2D[i,k] = temp;
+                }
+            }
+        }
     }
-  }
-  return array1D;
 }
 
-void Sort(int[] array)
-{
-  for (int i = 0; i < array.Length - 1; i++)
-  {
-    for (int j = i + 1; j < array.Length; j++)
-    {
-      if (array[i] > array[j]);
-      {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
-    }
-  }
-}
-void CountElements(int[] array)
-{
-    int count = 1;
-  for (int i = 0; i < array.Length; i++)
-  {
-    if (array[i] == array[i + 1]) ++count;
-    else
-    {
-      Console.WriteLine($"{array[i]} встречается {count} раз.");
-     count = 1;
-    }
-  }
-}
 
 Console.Clear();
-int[,] array2D = GetArray(3, 3, -10, 10);
-PrintArray(array2D);
-int[] array1D = Transform2DTo1D(array2D);
-Sort(array1D);
-CountElements(array1D);
-
+Console.Write("row = ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("col = ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] newArray = GetArray(row, col, 1, 10);
+Console.WriteLine();
+PrintArray(newArray);
+SortArray(newArray);
+Console.WriteLine();
+PrintArray(newArray);
---
+++ двумерный массив/задача 54/Program.cs
@@ -0,0 +1,55 @@
+﻿int[,] GetArray(int row, int col, int minValue, int maxValue)
+{
+  int[,] result = new int[row, col];
+
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Console.Write($"{inArray[i, j]}\t ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void SortArray(int[,] array2D)
+{
+  for (int i = 0; i < array2D.GetLength(0); i++)
+  {
+    for (int j = 0; j < array2D.GetLength(1) - 1; j++)
+    {
+      for (int k = j + 1; k < array2D.GetLength(1); k++)
+      {
+        if (array2D[i, j] < array2D[i, k])
+        {
+          int temp = array2D[i , j];
+          array2D[i, j] = array2D[i, k];
+          array2D[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+Console.WriteLine("row= ");
+int row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("col= ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] newArray = GetArray(row, col, 1, 10);
+Console.WriteLine();
+PrintArray(newArray);
+SortArray(newArray);
+Console.WriteLine();
+PrintArray(newArray);
