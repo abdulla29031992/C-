069a6db3e37adc0ca7bbef0ff5a7327908b98fee
unknown <mr.ad1992@mail.ru>
2023-07-24 14:28:31 +0300

    изменил код теста

--- test/Program.cs
+++ test/Program.cs
@@ -1,15 +1,1 @@
-﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
-// все натуральные числа в промежутке от 1 до N.
-// N = 5 -> "1, 2, 3, 4, 5"
-// N = 6 -> "1, 2, 3, 4, 5, 6"
-
-string NumberFor(int n)
-{
-if (n == 1) return $"{n}";
-return NumberFor(n - 1) + $", {n}";
-}
-
-Console.Clear();
-Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine(NumberFor(n));
+﻿
